fc7f80de283eb447341661282866eabaf0d62150
Nazaadp <nazarenoadp@gmail.com>
2026-02-21 18:25:15 -0300

    fixed domain for yts

--- server/src/services/YTSClient.cpp
+++ server/src/services/YTSClient.cpp
@@ -44,7 +44,8 @@ namespace media::services {
     // YTS Client Implementation
     class YTSClient::Impl {
     public:
-        const std::string BASE_URL = "https://yts.mx/api/v2";
+        const std::string BASE_URL = std::getenv("YTS_URL");
+        //const std::string BASE_URL = "https://yts.pm/api/v2";
 
         std::vector<DiscoveredContent> parseMovieList(const std::string& json_response) {
             std::vector<DiscoveredContent> results;
